9e4beb0c37ed3d9397d6d3c804d6394a178e01c5
Peter Tag <peter.h.tag@gmail.com>
2019-01-08 13:04:34 -0800

    Development

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// var StarWarsGame = {
+//     playerImages: ["Star-Wars-avatars-Movie-Luke-Skywalker-lightning.jpg"]
+// }
+ $(".player-images").on("click", function() {
+    
+    // We then output the number of times the crystal is clicked.
+    alert("You clicked this image");
+    
+});
+    
