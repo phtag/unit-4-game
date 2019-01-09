74dafe68a03cfb9c1d8848bcefc8ecce09307e9e
Peter Tag <peter.h.tag@gmail.com>
2019-01-09 11:04:31 -0800

    updates...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,38 @@
-// var StarWarsGame = {
-//     playerImages: ["Star-Wars-avatars-Movie-Luke-Skywalker-lightning.jpg"]
-// }
+
+
+var StarWarsGame = {
+    playerImages: ["Star-Wars-avatars-Movie-Luke-Skywalker-lightning.jpg",
+                    "Star-Wars-avatars-Movie-Obi-Wan-Kenobi-Alec-Guinness.jpg",
+                    "Star-Wars-avatars-Movie-Obi-Wan-Kenobi-Ewan-McGregor.jpg",
+                    "Star-Wars-avatars-Movie-Emperor-Papaltine-Darth-Sidious.jpg",
+                    "Star-Wars-avatars-Movie-Darth-Vader.jpg",
+                    "Star-Wars-avatars-Movie-Count-Dooku.jpg"],
+    playerNames: ["Luke Skywalker",
+                    "Ben Kenobi",
+                    "Obi Wan Kenobi",
+                    "Darth Sidious",
+                    "Darth Vader",
+                    "Count Dooku"]
+}
  $(".player-images").on("click", function() {
     
     // We then output the number of times the crystal is clicked.
     alert("You clicked this image");
     
 });
+
+var avatars = $("#avatars");
+for (i=0;i<StarWarsGame.playerImages.length;i++) {
+    var avatarImage = $('<img class="img-responsive player-images">');
+ 
+    avatars.append(avatarImage);
+    avatarImage.attr('src', "assets/images/" + StarWarsGame.playerImages[i]);
+    avatarImage.attr('avatar', StarWarsGame.playerNames[i]);
+    // letterBtn.text(letters[i]);
+
+}
+$('.player-images').on("click", function(){
+    alert("Got here=" + $(this).attr('avatar'));
+});
+
     
