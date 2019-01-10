ce2e28ce14cb19c1c78affe5ad5958abeb712aad
Peter Tag <peter.h.tag@gmail.com>
2019-01-10 15:47:28 -0800

    Work in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 
 
 var StarWarsGame = {
-    playerImages: ["Star-Wars-avatars-Movie-Luke-Skywalker-lightning.jpg",
+    characterImages: ["Star-Wars-avatars-Movie-Luke-Skywalker-lightning.jpg",
                     "Star-Wars-avatars-Movie-Obi-Wan-Kenobi-Alec-Guinness.jpg",
                     "Star-Wars-avatars-Movie-Obi-Wan-Kenobi-Ewan-McGregor.jpg",
                     "Star-Wars-avatars-Movie-Emperor-Papaltine-Darth-Sidious.jpg",
                     "Star-Wars-avatars-Movie-Darth-Vader.jpg",
                     "Star-Wars-avatars-Movie-Count-Dooku.jpg"],
-    playerNames: ["Luke Skywalker",
+    characterNames: ["Luke Skywalker",
                     "Ben Kenobi",
                     "Obi Wan Kenobi",
                     "Darth Sidious",
@@ -15,21 +15,47 @@ var StarWarsGame = {
                     "Count Dooku"]
 }
 
-var avatars = $("#avatars");
-var player = $("#player");
-for (i=0;i<StarWarsGame.playerImages.length;i++) {
-    var avatarImage = $('<img class="img-responsive player-images">');
- 
-    avatars.append(avatarImage);
-    avatarImage.attr('src', "assets/images/" + StarWarsGame.playerImages[i]);
-    avatarImage.attr('avatar', StarWarsGame.playerNames[i]);
+var characters = $("#characters");
+var charactersRowHeading = $(".my-characters-row-heading");
+var selectedCharacter = $("#selected-character");
+var enemiesToAttack = $("#enemies-to-attack");
+var myEnemiesRowHeading = $(".my-enemies-row-heading");
+myEnemiesRowHeading.html("");
+for (i=0;i<StarWarsGame.characterImages.length;i++) {
+    var imageContainer = $('<div class="image-container">');
+    var characterImage = $('<img class="img-responsive character-images">');
+    imageContainer.append(characterImage);
+    characters.append(imageContainer);
+    // characters.append(characterImage);
+    characterImage.attr('src', "assets/images/" + StarWarsGame.characterImages[i]);
+    characterImage.attr('character', StarWarsGame.characterNames[i]);
     // letterBtn.text(letters[i]);
 }
-$('.player-images').on("click", function(){
-    var playerImage = $('<img class="img-responsive player-images">');
-    player.html("");    // clear existing attributes and content for the div
-    player.append(playerImage);
-    playerImage.attr('src', $(this).attr('src'));
+$('.character-images').on("click", function(){
+    var imageContainer = $('<div class="image-container" <span style="color:blue;font-size:10px;top:-10px">12345</span>'); 
+    var characterImage = $('<img class="img-responsive character-images">');
+    imageContainer.append(characterImage);
+
+    selectedCharacter.html("");    // clear existing attributes and content for the div
+    selectedCharacter.append(imageContainer);
+    selectedCharacter.append('Your character');
+    characterImage.attr('src', $(this).attr('src'));
+
+    characters.html("");
+    charactersRowHeading.html("");
+    //  move the enemies to attack into position
+    enemiesToAttack.html("");
+    myEnemiesRowHeading.html("Enemies available to attack");
+    for (i=0;i<StarWarsGame.characterImages.length;i++) {
+        if ($(this).attr('src') != "assets/images/" + StarWarsGame.characterImages[i]) {
+            var imageContainer = $('<div class="image-container">');
+            var characterImage = $('<img class="img-responsive character-images">');
+            imageContainer.append(characterImage);
+            enemiesToAttack.append(imageContainer);
+                characterImage.attr('src', "assets/images/" + StarWarsGame.characterImages[i]);
+                characterImage.attr('character', StarWarsGame.characterNames[i]);    
+        }
+    }
 });
 
     
